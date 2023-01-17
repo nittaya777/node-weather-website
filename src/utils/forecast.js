54875781b6a60df898446cc1b5a3d3c54ace9841
Nittaya Thaengthong <nittaya.may31@gmail.com>
2023-01-17 16:57:33 +0700

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require("postman-request");
+const settings = require("../settings");
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=${settings.access_key}&query=${latitude},${longitude}&units=m`;
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service.", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        `${body.current.weather_descriptions}. It is current ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degress out.`
+      );
+    }
+  });
+};
+
+module.exports = forecast;
